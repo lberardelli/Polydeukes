a3db207e310762ab57d7ae3165796350ecd9e339
lawrence berardelli <lawrence.berardelli@gmail.com>
2024-11-24 13:41:46 -0500

    remove old layout

--- shaders/axisfs.glsl
+++
@@ -1,6 +0,0 @@
-#version 330 core
-in vec3 aColour;
-out vec4 FragColor;
-void main() {
-    FragColor = vec4(aColour.x, aColour.y, aColour.z, 1.0);
-}
--- shaders/passthroughvs.glsl
+++
@@ -1,7 +0,0 @@
-#version 410 core
-layout (location = 0) in vec3 VertexPosition;
-
-void main()
-{
-    gl_Position = vec4(VertexPosition, 1.0);
-}
